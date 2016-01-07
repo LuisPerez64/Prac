387f95958c4c656d7b8dc854468fc191d9f99f9d
Luis Perez <Luis_Perez64@live.com>
2016-01-07 13:34:59 -0500

    Modified the Lib Catalogue, reworking soon

--- Python/randomOnes/libraryCataloging/libraryCatalogue.py
+++ Python/randomOnes/libraryCataloging/libraryCatalogue.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.5
 '''
 Library cataloging Program
 
@@ -7,11 +8,15 @@ Books written by a certain author as well.
 Want to make this my first Graphical Python Program.
 ''' 
 import re #Regex management
+import os #Just for file checking
 catalogue=dict() #Key:Authors Name, Value: List Of Books
+duplicates=dict() #DUplicates dictionary
 manipFile = "Catalogue.txt" # File to be read and written to
+duplicatesFile = "Duplicates.txt" # Seperate file for Duplicates
 def main():
    
-    readIntoDict(manipFile)
+    readIntoDict(manipFile, catalogue)
+    readIntoDict(duplicatesFile, duplicates)
     print("Inputting a new value or searching the catalogue")
     print("(0) Quit \n(1) Input \n(2) Search\n")
     choice=input("Choice: ")
@@ -22,10 +27,14 @@ def main():
         rudimentarySearch()
     else:
         exit()
-    outputDict(manipFile)
+    outputDict(manipFile, catalogue)
+    outputDict(duplicatesFile, duplicates)
+
 '''
 Reads in the needed file, and logs all of the lines into a list, one by one
 Cleans up the end of each of the lists as well. Removing sole new lines as well
+Called By:
+  ReadIntoDict
 '''
 def importFile(fileName, inputList = None):
     toggle = False
@@ -43,9 +52,12 @@ def importFile(fileName, inputList = None):
 '''
 Gets the catalogued files, and brings them back into the fold, so as to make
 the catalogue expand as is needed, and when it is needed.
-
+Called by:
+  main
 '''
-def readIntoDict(fileName):
+def readIntoDict(fileName, currentDict):
+    if not os.path.isfile(fileName):
+        return #The file does not exist, don't try and read from it yet
     inputList = importFile(fileName)
     newAuthorRegex = ".+: #.+\d" #Enforce a format operation
     authorNameRegex = '.+:'
@@ -56,31 +68,41 @@ def readIntoDict(fileName):
         if q is not None: #New author found
             w = re.search('.+:', q.group(0))
             w = w.group(0).rstrip(':')
-            catalogue[w] = []
-        elif len(line) != 0:
-            catalogue[w].extend([line])
+            currentDict[w] = []
+        elif len(line) != 0: #Add in all the books written by Author
+            currentDict[w].extend([line]) # Ends when a sole newline is met
+
 '''
 New data is being appended to the given dictionary, no duplicates.
+Called by:
+  main
 '''
 def newInput():
     print("Beginning input of the needed materials")
     while True:
-        author=input("Input author name (~ to stop Input): ")
+        author=input("Input author name (~ to exit): ")
         if author == '~':
             break
         if author not in catalogue:
             catalogue[author] = [] #Append the new author
         while True:
-            book=input("Book Name (~ to stop input): ")
-            if book == '~': #Delimiter is met
+            book=input("Book Name (~ to stop book input): ")
+            if book == '~': #Delimiter is met, go back up to author level
                 break
-                if book not in catalogue[author]:
-                    catalogue[author].extend([book]) #Add book to authors list
-                else:
-                    print(book," already exists in the library")
-        
+            elif book not in catalogue[author]:
+                catalogue[author].extend([book]) #Add book to authors list
+            else:
+                print(book," already exists in the library")
+                if author not in duplicates: #Put the dups, in own dict
+                    duplicates[author] = [book]
+                if book in duplicates[author]:
+                    print("More than two copies of this book")
+                duplicates[author].extend([book])
+                                    
 '''
 Search function to be adapted when the time comes, and made graphical.
+Called by:
+  main
 '''
 def rudimentarySearch():
     query = input("What or Whom are you looking for: ")
@@ -96,8 +118,8 @@ def rudimentarySearch():
                 toggle = True
             else:
                 break
-
-        for value in catalogue[key]:
+        #This is more or less to see if looking for a book
+        for value in catalogue[key]: #Read the books by each author
             if query == value:
                 print("Book ", query, " by author ", key," found")
                 break #Found the book that was getting searched for
@@ -108,17 +130,19 @@ def rudimentarySearch():
 
 '''
 Output the contents of the updated Dictionary to the needed file.
+Called from
+  main
 '''
-def outputDict(fileName):
+def outputDict(fileName, currentDict):
     outString ='' #String stream object
     totalNumBooks = 0
     with open(fileName, 'w') as outFile:
-        for i in sorted(catalogue):
-            outString += i+ ": # of Books "+str(len(catalogue[i]))+'\n'
-            for k in catalogue[i]:
+        for i in sorted(currentDict):
+            outString += i+ ": # of Books "+str(len(currentDict[i]))+'\n'
+            for k in currentDict[i]:
                 outString += k +'\n'
             outString+= '\n'
-            totalNumBooks += len(catalogue[i])
+            totalNumBooks += len(currentDict[i])
         outString += '\n'+"Total Number of Books: " + str(totalNumBooks)
         outFile.write(outString)
 
